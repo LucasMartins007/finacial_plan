f707f9ef7c955bb713940e912ad6197296099d37
Lucas martins <lucasrodriguesmartinsoliveira@gmail.com>
2023-06-25 23:17:49 -0300

    iniciando tela inicial de login

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { LoginService } from '../shared/services/login/login.service';
 import { LoginComponent } from './login/login.component';
 
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule,
+    
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatListModule,
+  ],
+  providers: [LoginService],
+  exports: [LoginComponent],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginFormStyle: string = 'login-form';
+  loginFormGroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginFormGroup = this.formBuilder.group({
+      username: [''],
+      senha: [''],
+    });
   }
 
+  ngOnInit(): void {}
 }
